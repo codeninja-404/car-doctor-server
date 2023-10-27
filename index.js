a69a5c0a314f8e952ed4eccaacff676f86373fb1
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-10-27 08:48:59 +0600

    random

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,8 +10,7 @@ app.use(cors());
 app.use(express.json());
 console.log();
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.huafxe1.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.huafxe1.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,6 +25,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const servisesCollection = client.db("carDoctor").collection("servises");
+
+    app.get("/servises", async (req, res) => {
+      const cursor = servisesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/servises/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection:{  title:1, price:1, service_id:1 }
+      }
+      const result = await servisesCollection.findOne(query,options);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
